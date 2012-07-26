8645663441b5296687035ba295792f4638eba9f7
Peter Wilson <peter.t.wilson@gmail.com>
2012-07-25 17:58:23 -0700

    Add slider, spinner & progress bar examples.

---
+++ listing07_9.py
@@ -0,0 +1,26 @@
+import wx
+
+class GaugeFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, 'Gauge Example', size=(350, 150))
+        panel = wx.Panel(self, -1)
+        self.count = 0
+        self.gauge = wx.Gauge(panel, -1, 25, (20, 50), (250, 25))
+        self.gauge.SetBezelFace(3)
+        self.gauge.SetShadowWidth(100)
+        self.Bind(wx.EVT_IDLE, self.OnIdle)
+
+    def OnIdle(self, event):
+        self.count = self.count + 1
+        if self.count >= 50:
+            self.count = 0
+        print self.count
+        self.gauge.SetValue(self.count)
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    GaugeFrame().Show()
+    app.MainLoop()
+
+
