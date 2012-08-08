f2283eca62edd52c6353fa077dc54a25d6d28e2e
Peter Wilson <peter.t.wilson@gmail.com>
2012-08-07 18:03:20 -0700

    First 6 examples from chapter 11.

---
+++ listing11_06.py
@@ -0,0 +1,21 @@
+import wx
+from listing11_01 import BlockWindow
+
+labels = "one two three four five six seven eight nine".split()
+
+class TestFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, "FlexGridSizer Test")
+        sizer = wx.FlexGridSizer(rows=3, cols=3, hgap=5, vgap=5)
+        for label in labels:
+            bw = BlockWindow(self, label=label)
+            if label == "five": bw.SetMinSize((150,50))
+            sizer.Add(bw, 0, wx.ALL, 5)
+        # center = self.FindWindowByName("five")
+        # center.SetMinSize((150,50))
+        self.SetSizer(sizer)
+        self.Fit()
+
+app = wx.PySimpleApp()
+TestFrame().Show()
+app.MainLoop()
